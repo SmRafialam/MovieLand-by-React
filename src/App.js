3829359c447e39ba8ddfbb3054fd986c1b8b56c2
SmRafialam <smrafialam@gmail.com>
2023-11-01 18:52:33 +0600

    design MovieLand page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { ReactDOM } from 'react-dom';
+import { useEffect } from 'react';
+import SearchIcon from './search.svg';
 
-function App() {
+//api key: b10ddd7c
+
+const API_URL = 'http://www.omdbapi.com?apikey=b10ddd7c'
+
+// const PersonComponent = (props) => {
+//   return (
+//     <>
+//       <h2>Name: {props.name}</h2>
+//       <h2>LastName: {props.lastname}</h2>
+//       <h2>Age: {props.age}</h2>
+//     </>
+//   )
+// }
+
+const App = () => {
+  // const[counter, setCounter] = useState(0);
+  const searchMovies = async(title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input 
+          placeholder='Search for movies'
+          value="Superman"
+          onChange={()=>{}}
+        />
+        <img 
+          src={SearchIcon}
+          alt='search'
+        />
+      </div>
+      {/* <button onClick={()=> setCounter((prevCount)=> prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=> setCounter((prevCount)=> prevCount+1)}>+</button>
+
+      <PersonComponent
+        name="Mery" 
+        lastname={'Ana'} 
+        age={30} 
+      ></PersonComponent>
+      <PersonComponent 
+        name="john" 
+        lastname={'Doe'} 
+        age={30} 
+      /> */}
     </div>
   );
 }
